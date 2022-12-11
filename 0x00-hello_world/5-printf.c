a62a57457250b50d84fca3400c85f099508ebe41
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-11 03:18:19 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+
 /**
- * The prog should print with proper grammar, but the outcome is a piece of art,
+ * The prog should print the line given
  */
 
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
