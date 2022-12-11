e55ca00deb431a205482f1d7b137f201c9629f98
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-11 03:12:00 -0800

    task 0 - 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * The prog should print with proper grammar, but the outcome is a piece of art,
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
