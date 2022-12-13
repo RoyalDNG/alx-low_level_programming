2ca3c57d04113bf616eb27738456c98e6fb11afe
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-12 16:41:33 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - print base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	while(num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	return (0);
+}
