8dc3fa7c3568389a4ce949f80a4c431791752c34
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-12 18:35:48 -0800

    task-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int a = 0;
-	while (a < 10)
-	{
+	while
+	       	(a < 10){
 		putchar(a + '0');
 		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
 		a++;
 	}
+	putchar('\n');
 	return (0);
 }
