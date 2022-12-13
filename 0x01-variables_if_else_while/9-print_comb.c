cdee2f0ea9af67367885654fabca60d3f5b74da4
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-12 18:26:48 -0800

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - the noms
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+	return (0);
+}
