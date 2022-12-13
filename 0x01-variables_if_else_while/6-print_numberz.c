97d660a056ddd26660967648690933a98f1e264f
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-12 16:58:57 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - print using
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
