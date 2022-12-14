d75b89c515a37a10226e40f7ac9757a8af8d2d19
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-14 15:06:57 -0800

    commie

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - does as it says
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int t = 0;
+	
+	while (t < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		t++;
+		_putchar('\n');
+	}
+}
