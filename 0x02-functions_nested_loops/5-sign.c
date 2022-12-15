33a3fb26a145cefd44e259f0072a82222e36fcc9
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-15 11:14:20 -0800

    commie

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - sign
+ * @n: number
+ * Return: 1 positive, -1 negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
