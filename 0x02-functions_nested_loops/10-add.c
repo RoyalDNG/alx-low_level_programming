f3ad2076f386de9cbd668673f92ba8173b0f14d7
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-15 15:56:54 -0800

    commie

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition of two numbers
+ * a, b: these are two numbers
+ * Retur: There sum
+ */
+int add(int a, int b)
+{
+	int s;
+	s = a + b;
+	_putchar(s);
+	return (s);
+}
