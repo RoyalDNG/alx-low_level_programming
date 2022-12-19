340cfdfbb92d0d19265058eb5ffae1356b55db81
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-19 10:21:56 -0800

    commie

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - exchanges the values pointed at by to pointers
+ * @a: is the first one
+ * @b: is the second
+ */
+void swap_int(int *a, int *b)
+{
+	int holder;
+	holder = *a;
+	*a = *b;
+	*b = holder;
+}
