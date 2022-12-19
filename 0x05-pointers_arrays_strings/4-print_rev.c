6659fe0f2f62beb50cc169a2c399c3af83a75a2a
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-19 13:18:09 -0800

    commie

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: is the characters of the string
+ */
+void print_rev(char *s)
+{
+	int count;
+	
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	while (count >= 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+}
