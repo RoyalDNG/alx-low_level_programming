153ae4e04b1ca0679241aa239d78774b7cf6caad
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-19 10:31:50 -0800

    commie

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - gives the length of a given string
+ * @s: is the pointer to string of characters
+ * Return: number of characters
+ */
+int _strlen(char *s)
+{
+	int count;
+	
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	return (count);
+}
