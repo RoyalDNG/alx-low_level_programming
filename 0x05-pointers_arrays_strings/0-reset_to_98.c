226bd2dff5835d58bd73bcd2094d9c0f9dba5fdb
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-19 10:13:44 -0800

    lmao

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function accepts a pointer and changes the
+ * it points to to 98
+ * @*n: this is the pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
