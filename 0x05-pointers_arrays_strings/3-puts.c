b24ed6f54ae0df68c49c032f01c8c44656556565
Royal_DNG <dennisnjenga45@gmail.com>
2022-12-19 10:55:45 -0800

    commie

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - sends the string to stdout
+ * @str: this char set of string
+ */
+void _puts(char *str)
+{
+	int index;
+	for (index = 0; str != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
